3fd25daca72e9259e085a9b5dd1eb0b566e2a1f0
odutradev <joao.vitornl@gmail.com>
2026-03-10 10:31:38 -0300

    feat: Adds admin role and implements automatic bypass in hasrole for super role

--- src/middlewares/hasRole.ts
+++ src/middlewares/hasRole.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
+import { SUPER_ROLES } from "@utils/constants/roles";
 import sendError from "@utils/functions/error";
 import userModel from "@database/model/user";
 import logger from "@utils/functions/logger";
@@ -7,31 +8,37 @@ import logger from "@utils/functions/logger";
 import type { RoleType } from "@utils/constants/roles";
 
 const hasRole = (allowedRoles: RoleType[]) => async (req: Request, res: Response, next: NextFunction): Promise<void> => {  
-  try {
-    const userID = res.locals?.userID;
-    
-    if (!userID) {
-      return sendError({ code: "invalid_credentials", res });
-    }
+    try {
+        const userID = res.locals?.userID;
+        
+        if (!userID) {
+            sendError({ code: "invalid_credentials", res });
+            return;
+        }
 
-    const user = await userModel.findById(userID).select("role");
-    
-    if (!user) {
-      return sendError({ code: "admin_access_denied", res });
-    }
+        const user = await userModel.findById(userID).select("role");
+        
+        if (!user) {
+            sendError({ code: "admin_access_denied", res });
+            return;
+        }
 
-    const hasPermission = allowedRoles.includes(user.role as RoleType);
-    
-    if (!hasPermission) {
-      return sendError({ code: "admin_access_denied", res });
-    }
+        const role = user.role as RoleType;
+        const isSuperUser = SUPER_ROLES.includes(role);
+        const hasPermission = allowedRoles.includes(role);
+        
+        if (!isSuperUser && !hasPermission) {
+            sendError({ code: "admin_access_denied", res });
+            return;
+        }
 
-    return next();
-  } catch (error) {
-    logger.error("[hasRole] Role validation internal error");
-    console.log(error);
-    return sendError({ code: "internal_error", res });
-  }
+        next();
+    } catch (error) {
+        logger.error("[hasRole] Role validation internal error");
+        console.log(error);
+        sendError({ code: "internal_error", res });
+        return;
+    }
 };
 
 export default hasRole;
--- src/utils/constants/roles.ts
+++ src/utils/constants/roles.ts
@@ -1,17 +1,24 @@
 export const ROLES = {
-    DIRETOR_PRESIDENTE: "diretor_presidente",
-    DIRETOR_VICE_PRESIDENTE: "diretor_vice_presidente",
-    DIRETOR_FINANCEIRO: "diretor_financeiro",
     DIRETOR_SUPRIMENTOS_CONTRATOS: "diretor_suprimentos_contratos",
-    DIRETOR_MANUTENCAO: "diretor_manutencao",
-    DIRETOR_ADMINISTRATIVO_RH: "diretor_administrativo_rh",
     DIRETOR_PRODUCAO_AGROPECUARIA: "diretor_producao_agropecuaria",
-    DIRETOR_TI_MARKETING: "diretor_ti_marketing",
+    DIRETOR_ADMINISTRATIVO_RH: "diretor_administrativo_rh",
+    DIRETOR_VICE_PRESIDENTE: "diretor_vice_presidente",
     ASSESSOR_TECNICO_JURIDICO: "assessor_tecnico_juridico",
+    DIRETOR_TI_MARKETING: "diretor_ti_marketing",
+    DIRETOR_PRESIDENTE: "diretor_presidente",
+    DIRETOR_MANUTENCAO: "diretor_manutencao",
+    DIRETOR_FINANCEIRO: "diretor_financeiro",
     MEMBRO_ASSEMBLEIA: "membro_assembleia",
-    NORMAL: "normal"
+    NORMAL: "normal",
+    ADMIN: "admin"
 } as const;
 
-export const ROLES_ARRAY = Object.values(ROLES);
-
 export type RoleType = typeof ROLES[keyof typeof ROLES];
+
+export const ROLES_ARRAY: RoleType[] = Object.values(ROLES);
+
+export const SUPER_ROLES: RoleType[] = [
+    ROLES.DIRETOR_VICE_PRESIDENTE,
+    ROLES.DIRETOR_PRESIDENTE,
+    ROLES.ADMIN
+];
