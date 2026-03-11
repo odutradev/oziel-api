aea9a495b04252b1bae3fc653e6158f37e733cc3
odutradev <joao.vitornl@gmail.com>
2026-03-11 17:19:06 -0300

    feat: Creating log endpoint

---
+++ src/routes/resources/logs.router.ts
@@ -0,0 +1,20 @@
+
+import { Router } from "express";
+
+import logsResource from "@resources/logs/logs.resource";
+import manageRequest from "@middlewares/manageRequest";
+import hasRole from "@middlewares/hasRole";
+import auth from "@middlewares/auth";
+
+const logsRouter = Router();
+
+logsRouter.get("/user/me", manageRequest(logsResource.getUserLogs));
+logsRouter.get("/entity/:entityID", manageRequest(logsResource.getEntityLogs));
+logsRouter.get("/action/:action", [hasRole([])], manageRequest(logsResource.getActionLogs));
+
+logsRouter.get("/admin/activity", [hasRole([])], manageRequest(logsResource.getSystemActivity));
+logsRouter.get("/admin/errors", [hasRole([])], manageRequest(logsResource.getErrorLogs));
+logsRouter.get("/admin/stats", [hasRole([])], manageRequest(logsResource.getLogStats));
+logsRouter.get("/admin/all", [hasRole([])], manageRequest(logsResource.getAllLogs));
+
+export default logsRouter;
