8293fedde277ffaa746e7a69a8145b6e1402eb5a
odutradev <joao.vitornl@gmail.com>
2026-03-11 17:21:42 -0300

    feat: Making new endpoints available

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,8 @@ import treasuryRouter from "./resources/treasury.router";
 import controlAccess from "@middlewares/controlAccess";
 import usersRouter from "./resources/users.router";
 import vaultRouter from "./resources/vault.router";
+import emailsRouter from "./resources/emails.router";
+import logsRouter from "./resources/logs.router";
 
 const router = Router();
 
@@ -20,5 +22,7 @@ router.use("/recurring-transactions", [controlAccess], recurringTransactionRoute
 router.use("/treasury", [controlAccess], treasuryRouter);
 router.use("/vaults", [controlAccess], vaultRouter);
 router.use("/users", [controlAccess], usersRouter);
+router.use("/emails", [controlAccess], emailsRouter);
+router.use("/logs", [controlAccess], logsRouter);
 
 export default router;
